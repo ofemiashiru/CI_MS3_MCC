434cdd0dc5246dcc49a2ef579f1322d070e4aa02
ofemiashiru <>
2023-07-16 18:26:28 +0000

    feat: allow admin to update genre/allow it to effect all movies

--- app.py
+++ app.py
@@ -257,10 +257,20 @@ def edit_genre(genre_id):
     Route used to edit a single genre
     """  
     if "user" in session and session["user"].lower() == "admin":
+        # Grabs the previous genre and stores it
+        prev_genre = mongo.db.genres.find_one({"_id": ObjectId(genre_id)})
         if request.method == "POST":
             updated_genre = {
                 "genre_name" : request.form.get("genre_name")
             }
+            mongo.db.genres.update_one(
+                {"_id": ObjectId(genre_id)}, {"$set": updated_genre})
+            # Uses the previous genre to update all associated movies
+            mongo.db.movies.update_many(
+                {"genre_name" :  prev_genre['genre_name']}, {"$set" : updated_genre})
+
+            flash("Your genre has been succesfully updated")
+            return redirect(url_for("show_genres"))
         genre = mongo.db.genres.find_one({"_id": ObjectId(genre_id)})
         return render_template("edit_genre.html", genre=genre)
 
