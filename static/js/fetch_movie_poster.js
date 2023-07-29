3e00dcbe990e6177721fef054320f3f00b20b0db
ofemiashiru <>
2023-07-29 14:34:19 +0000

    docs: upload no image place holder

--- static/js/fetch_movie_poster.js
+++ static/js/fetch_movie_poster.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     
     //Retrieves api key 
     const getKey = function(){
-        return btoa(encodedKey())
-    }
+        return btoa(encodedKey());
+    };
 
     // Get Movie cover - add_movie.html
     const fetchMovieCover = function(){
@@ -11,26 +11,28 @@ document.addEventListener("DOMContentLoaded", function () {
         document.querySelector("#movie-img").src = "";
         document.querySelector("#poster").value = "";
         const movieName = document.querySelector("#title").value;
-
+        document.querySelector(".preloader-wrapper").classList.remove("hidden");
 
         if(!movieName){
             document.querySelector(".movie-cover-container__header").innerHTML = "Please enter a title";
             document.querySelector("#poster").value = "Please enter a title";
+            document.querySelector("#movie-img").setAttribute("src", "./static/images/no-image-solid.svg");
+            document.querySelector(".preloader-wrapper").classList.add("hidden");
             return;  
         }
         // split movie name by space
         const arrMovieName = movieName.split(" ");
         // join movie name as a string with + between
         const strMovieName = arrMovieName.join("+");
-
         // fetch request
         fetch(`https://www.omdbapi.com/?t=${strMovieName}&apikey=${getKey()}`)
         .then(response => response.json())
         .then(data => {
             if(data.Response === "False"){
                 document.querySelector("#poster").value = "Poster not found!";
+                document.querySelector("#movie-img").setAttribute("src", "./static/images/no-image-solid.svg");
                 document.querySelector(".movie-cover-container__header").innerHTML = "Poster not found!";
-                document.querySelector(".preloader-wrapper").classList.remove("hidden");
+                document.querySelector(".preloader-wrapper").classList.add("hidden");
                 
             } else {
                 const omdbMoviePoster = data.Poster;
